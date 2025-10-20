d721965e16fde5af6f618c501ae6fea0f9bbdc04
Jelmond <ant.soloviov@gmail.com>
2025-10-20 17:12:24 +0300

    feat: update domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['mppshop.by', 'api.mppshop.by', '127.0.0.1'],
+    domains: ['mppshop.by', 'api.mppshop.by', '127.0.0.1', 'mppshop.by.'],
   },
   async rewrites() {
     return [
