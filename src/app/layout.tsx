89957b06b4c5ae010f7d8edc5c2354afb1981d21
Jelmond <ant.soloviov@gmail.com>
2026-01-15 17:24:22 +0300

    feat: add yandex script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,6 +72,23 @@ export default function RootLayout({
   return (
     <html lang="ru" translate="no">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17871808122"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17871808122');
+            `,
+          }}
+        />
+        {/* /Google tag (gtag.js) */}
+        
         {/* Yandex.Metrika counter */}
         <script
           type="text/javascript"
